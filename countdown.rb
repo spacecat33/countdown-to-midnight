2ad52cb57e1cba5a7c45b5ec77ed973bebbe3912
Janan Akkad <jananakkad@hotmail.co.uk>
2022-04-06 17:33:57 +0100

    Completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(number)
+        number = 10
+          while number > 0
+          puts "#{number} SECOND(S)!" 
+          number -= 1 
+        end
+        "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num_secs)
+    num_secs = 10
+    while num_secs > 0
+        sleep 1
+        puts "#{num_secs} SECOND(S)!"
+        num_secs -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
